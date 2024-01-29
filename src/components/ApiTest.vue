4d115bb8930d1f5d072c0af5eaeebaf56c337d03
Lucieguerin431 <lucie.guerin@edu.univ-eiffel.fr>
2024-01-29 20:39:53 +0100

    set up + premiers tests

---
+++ src/components/ApiTest.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  
+})
+</script>
+
+<template>
+    <div>
+        coucou je suis lapi de lulu
+    </div>
+</template>
+
+
+<style>
+</style>
+
+<script>
+import { createClient } from 'pexels';
+
+const client = createClient('udjXtzs8O2CXWR2aBuB4yqbHj9RF7zXaAoGAxTXOiAD6U9DsOmhw6USB');
+
+client.photos.show({ id: 3225517 }).then(photo => 
+    console.log(photo))
+
+
+</script>
